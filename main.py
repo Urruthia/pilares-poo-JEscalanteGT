ec2a0eb37c06b288d08be2aadfa92df73246ec97
Facutad de Ingenieria <ingenieria@LaboratoriosCQ.com.gt>
2023-05-17 12:54:08 -0500

    commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""
 from city import City
 
 city1 = City(
@@ -5,3 +6,15 @@ city1 = City(
     14.840553,
     -91.5179788
 )
+"""
+from perro import Dog
+from gato import Cat
+from animal import Animal
+cat1 = Cat('misho', 2, True)
+cat2 = Cat('mish', 3, False)
+cat3 = Cat('mishi', 4, True)
+dog1 = Dog('chucho', 3, 'Do comedia')
+dog2 = Dog('chucho1', 4, 'alguien')
+dog3 = Dog('chucho2', 5, 'alguien2')
+
+animals: list[Animal]=[cat1, cat2, cat3, dog1,dog2, dog3]
--- temperature.py
+++ temperature.py
@@ -1,4 +1,15 @@
+from datetime import datetime
+
+
 class Temperature:
-    def __init__(self, measure: float):
+    def __init__(self, measure: float, created_at: datetime):
         self.__measure = measure
         self.__unit = 'F'
+        self.__created_at = created_at
+
+    def show(self) -> str:
+        return f''\ 
+               f'Temperatura:{self.__measure}'\ 
+               f'{self.__unit}'\
+               f'Hora: {self.__created_at}'
+
